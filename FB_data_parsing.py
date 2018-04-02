ddbdf4511effa92a9d71bf068affad21c0ce6370
worlikarshweta <worlikar.shweta@gmail.com>
2018-04-02 14:43:48 -0400

    Update - Shweta Worlikar
    Update 
    To manage raw and processed files
    Process only 30 minuets old files
    loop hourly execution of the file

--- FB_data_parsing.py
+++ FB_data_parsing.py
@@ -208,12 +208,34 @@ def write_and_insert_processed_data(file):
 
 def process(file):
     write_and_insert_processed_data(file)
+  
+def process(root_dirc):
+    dirc = root_dirc + "download/"
+    for root, dirs, files in os.walk(dirc):
+        for file in files:
+            filename = root+"/"+file
+            t = time.time() - 30 * 60
+            if os.path.getatime(filename) < t:
+                new_filename = root_dirc+"processed/"+file
+                new_filename = new_filename.replace(".json", "_processed.json")
+                write_and_insert_processed_data(filename,new_filename)
+                raw_filename = root_dirc+"raw/"+file
+                os.rename(filename, raw_filename)
+               
+               
+def run_timeline(base_dirc):
+    while True:
+        process(base_dirc)
+        print('Job completed. Resuming in an hour.')
+        time.sleep(60 * 60)
 
 
 # The following is used to test
 
-test_file = ''
-process(test_file)
+#Need to create folders "raw" and "processed" in this base directory which has "download" folder with all the original files
+base_dirc = ''
+run_timeline(base_dirc)
+
 '''
 startdir = ''
 file_list = os.listdir(startdir)

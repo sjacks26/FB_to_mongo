d1b89b1d3b516a6ad2234201dfbe8e612cba0065
sjacks26 <sjacks26@gmail.com>
2018-06-13 16:01:25 -0400

    Add loop for file encoding problems. Add function to resolve key error

--- create_candidate_info_json.py
+++ create_candidate_info_json.py
@@ -6,6 +6,20 @@ candidate_info_json_file = cfg.candidate_info_json_file
 base_dirc = cfg.base_dirc
 
 
+def resolve_key_id(data):
+    """
+    For some reason, some FB data uses "page_id" and some uses "id" for the id number for the page it comes from.
+     This function prevents errors stemming from this.
+    """
+    key_id = ''
+    keys = list(data.keys())
+    if "id" in keys:
+        key_id = "id"
+    elif "page_id" in keys:
+        key_id = "page_id"
+    return key_id
+
+
 def generate_candidate_names_dict():
     """
     This function automatically generates the dict object used to associate files with particular candidates.
@@ -19,10 +33,15 @@ def generate_candidate_names_dict():
     candidate_names = {}
     with open(candidate_info_json_file, 'w') as o:
         for f in page_files:
-            with open(f, 'r') as i:
-                page_file_contents = json.load(i)
-                candidate_names[int(page_file_contents['id'])] = page_file_contents['name']
-                write_line = '{0}: {1}\n'.format(int(page_file_contents['id']), page_file_contents['name'])
+            try:
+                with open(f, 'r') as i:
+                    page_file_contents = json.load(i)
+            except ValueError:
+                with open(f, 'r', encoding='latin-1') as i:
+                    page_file_contents = json.load(i)
+                key_id = resolve_key_id(page_file_contents)
+                candidate_names[int(page_file_contents[key_id])] = page_file_contents['name']
+                write_line = '{0}: {1}\n'.format(int(page_file_contents[key_id]), page_file_contents['name'])
                 o.write(write_line)
 
 
